3dc13ada9547620aab82bd3e5d13c20815c59d03
YassierBashier <yassiergania09@gmail.com>
2021-07-23 11:42:50 +0800

    remove pop up in post job  card

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -56,26 +56,9 @@
       </q-card-section>
 
       <q-card-actions align="center">
-        <q-btn label="Post Job" color="primary" @click="alert = true" />
+        <q-btn label="Post Job" color="primary" v-close-popup />
 
-        <q-dialog v-model="alert">
-          <q-card>
-            <q-card-section>
-              <div class="text-h6">Job is waiting to be approved by the moderator</div>
-            </q-card-section>
-
-            <!-- <q-card-section class="q-pt-none">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-              repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis
-              perferendis totam, ea at omnis vel numquam exercitationem aut,
-              natus minima, porro labore.
-            </q-card-section> -->
-
-            <q-card-actions align="right">
-              <q-btn flat label="OK" color="primary" v-close-popup />
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
+       
       </q-card-actions>
     </q-card>
   </q-dialog>
