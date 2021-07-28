3fd84a801dd77f3c1ad97606a5d293748e735877
ruzlicali16 <ruzlicali24@gmail.com>
2021-07-28 23:01:45 +0800

    add font + create landing layout + landing page

---
+++ src/layouts/LandingLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-layout view="hhh LpR lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title
+          class="row items-center cursor-pointer"
+          @click="$router.replace('/home')"
+        >
+          <img class="q-pl-md" src="~/assets/Esugo2.png" height="25px" />
+        </q-toolbar-title>
+        <q-space />
+        <div class="q-gutter-x-md">
+          <q-btn
+            color="white"
+            text-color="primary"
+            icon-right="how_to_reg"
+            label="SIgnup"
+            to="signup"
+          />
+          <q-btn
+            color="white"
+            text-color="primary"
+            icon-right="login"
+            label="Login"
+            to="/login"
+          />
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component({})
+export default class EmployerLayout extends Vue {}
+</script>
--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,25 +1,56 @@
 <template>
   <q-page>
-    <SearchToolbar />
+    <div class="__container">
+      <!-- cover photo -->
+      <div class="__cover" :style="`height: ${screenHeight}px`">
+        <div class="__container row">
+          <div class="col-6 q-pl-xl flex flex-center text-h3 text-white">
+            <div>
+              <div>Hire A Vetted Team To Help You With Your Needs</div>
+              <div class="q-pt-md">
+                <q-btn
+                  class="text-weight-regular text-left"
+                  push
+                  rounded
+                  color="white"
+                  text-color="primary"
+                  label="Sign up now"
+                  size="lg"
+                  to="/signup"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="col-6 flex flex-center">
+            <div>photo</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
-import SearchToolbar from 'components/SearchToolbar.vue';
+import {Vue, Component} from 'vue-property-decorator';
 
-@Component({
-  components: {
-    SearchToolbar
+@Component({})
+export default class LandingPage extends Vue {
+  screenHeight = 0;
+
+  created() {
+    this.screenHeight = window.screen.height - 160;
+    console.log(this.screenHeight);
   }
-})
-export default class LandingPage extends Vue {}
+}
 </script>
 
 <style scoped>
-.q-page {
-  display: flex;
-  flex-flow: column;
+.__cover {
+  background: linear-gradient(to bottom left, #0066eb 0%, #ff8ab3 100%);
+  padding: 0px 100px 0px 100px;
+}
+
+.__cover .__container {
   height: 100%;
 }
 </style>
